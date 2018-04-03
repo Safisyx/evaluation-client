8ad99bf99caafd3b57be04c6628b4e2606fe055e
Safisyx <safisyx@gmail.com>
2018-04-03 23:03:15 +0200

    Add actions for login and logout

---
+++ src/actions/users.js
@@ -0,0 +1,31 @@
+import * as request from 'superagent'
+import {baseUrl} from '../constants'
+
+import {USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAILED, USER_LOGOUT} from './types'
+
+export const logout = () => ({
+  type: USER_LOGOUT
+})
+
+export const login = (email, password) => (dispatch) =>
+	request
+		.post(`${baseUrl}/logins`)
+    .send({email, password})
+    .then(result => {
+      dispatch({
+        type: USER_LOGIN_SUCCESS,
+        payload: result.body
+      })
+    })
+    .catch(err => {
+    	if (err.status === 400) {
+    		dispatch({
+    			type: USER_LOGIN_FAILED,
+    			payload: err.response.body.message || 'Unknown error'
+    		})
+    	}
+    	else {
+    		console.error(err)
+    	}
+    })
