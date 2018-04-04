3b7d205d91c0f1907d2d588381883bc3ad7f4b5c
Safisyx <safisyx@gmail.com>
2018-04-05 00:59:02 +0200

    Add a bar representing the percentage of colors

--- src/components/batches/BatchOverview.js
+++ src/components/batches/BatchOverview.js
@@ -11,23 +11,42 @@ class BatchOverview extends PureComponent {
       this.props.getBatch(this.props.match.params.id)
     }
   }
-
+  divStyle = (number)=> {
+    const w=Math.round(number)*6
+    console.log(w);
+    return {
+      width:`${w}px`
+    }
+  }
   render() {
-    const {batch, authenticated} = this.props
-
+    const {authenticated,batch} = this.props
+    if (!batch.colorsPercentage) return "loading.."
+    const colorsPercentage = batch.colorsPercentage
+    console.log(colorsPercentage);
     if (!authenticated) return (
 			<Redirect to="/login" />
 		)
 
-    if (this.props.pass) {
-      window.location.reload()
+    return (
+      <div className='BatchOverview'>
+        <div className='Progress'>
+          <div id='green' style={this.divStyle(colorsPercentage.green)}>
+            <p>{`${colorsPercentage.green}%`}</p>
+          </div>
+          <div id='yellow' style={this.divStyle(colorsPercentage.yellow)}>
+            <p>{`${colorsPercentage.yellow}%`}</p>
+          </div>
+          <div id='red' style={this.divStyle(colorsPercentage.red)}>
+            <p>{`${colorsPercentage.red}%`}</p>
+          </div>
+        </div>
+
+        <div className='students'>
+        </div>
+      </div>
+    )
     }
-
-    if (batch === null) return null
-    return null
-
   }
-}
 
 const mapStateToProps = state => ({
   authenticated: (state.login.user !== null) && (state.login.user !== undefined),
