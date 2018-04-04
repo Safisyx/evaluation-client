e62f13fb0fd8b45beec6cc85d9230f6ee9100d9b
Safisyx <safisyx@gmail.com>
2018-04-04 17:57:26 +0200

    Add a form for creating a batch

---
+++ src/components/batches/BatchForm.js
@@ -0,0 +1,49 @@
+import React, {PureComponent} from 'react'
+import './BatchForm.css'
+
+export default class BatchForm extends PureComponent {
+	state = {}
+
+	handleSubmit = (e) => {
+		e.preventDefault()
+		this.props.onSubmit(this.state)
+	}
+
+	handleChange = (event) => {
+    const {name, value} = event.target
+
+    this.setState({
+      [name]: value
+    })
+		console.log(this.state);
+  }
+
+	render() {
+		return (
+			<form className='LoginForm' onSubmit={this.handleSubmit}>
+				<div>
+					<label htmlFor='id'>Id</label>
+					<input type='number' name='id' id='id' value={
+						this.state.id || ''
+					} onChange={ this.handleChange } />
+				</div>
+
+				<div>
+					<label htmlFor='startDate'>{"Start Date"}</label>
+					<input type='date' name='startDate' id='startDate' value={
+						this.state.startDate || ''
+					} onChange={ this.handleChange } />
+				</div>
+
+				<div>
+					<label htmlFor='endDate'>{"Start Date"}</label>
+					<input type='date' name='endDate' id='endDate' value={
+						this.state.endDate || ''
+					} onChange={ this.handleChange } />
+				</div>
+
+				<button type='submit'>Add</button>
+			</form>
+		)
+	}
+}
