8e9ab97fdff7c7124c2ec6d19b49d1a13facab2e
Safisyx <safisyx@gmail.com>
2018-04-05 19:02:46 +0200

    Update state after adding evaluation

--- src/reducers/batch.js
+++ src/reducers/batch.js
@@ -1,4 +1,4 @@
-import {GET_BATCH, DELETE_STUDENT, ADD_STUDENT, EDIT_STUDENT} from '../actions/types'
+import {GET_BATCH, DELETE_STUDENT, ADD_STUDENT, EDIT_STUDENT, ADD_EVALUATION} from '../actions/types'
 
 export default (state = {}, {type, payload}) => {
   switch (type) {
@@ -28,6 +28,41 @@ export default (state = {}, {type, payload}) => {
           return student
         })
       }
+
+    case ADD_EVALUATION:
+    console.log('---ADD_EVALUATION---');
+    console.log(payload);
+      if (payload.post) {
+        return {
+          ...state,
+          students: state.students.map(student => {
+            if (student.id===payload.post.studentId) {
+              return {
+                ...student,
+                evaluations: student.evaluations.concat(payload.post)
+              }
+            }
+            return student
+          })
+        }
+      }
+      return {
+        ...state,
+        students: state.students.map(student => {
+          if (student.id===payload.patch.studentId) {
+            return {
+              ...student,
+              evaluations: student.evaluations.map(evaluation => {
+                if (evaluation.id===payload.patch.id)
+                  return payload.patch
+                return evaluation
+              })
+            }
+          }
+          return student
+        })
+      }
+
     default:
       return state
   }
--- src/reducers/errors.js
+++ src/reducers/errors.js
@@ -1,5 +1,6 @@
 import {ADD_BATCH_ERROR, ADD_BATCH,
-        ADD_STUDENT, ADD_STUDENT_ERROR} from '../actions/types'
+        ADD_STUDENT, ADD_STUDENT_ERROR,
+        ADD_EVALUATION, ADD_EVALUATION_ERROR} from '../actions/types'
 
 /*
 The state will contain the batches in an object with the ID as key
@@ -20,6 +21,14 @@ export default (state = {}, {type, payload}) => {
         error:payload
       }
     case ADD_STUDENT:
+      return {
+        pass: 'OK'
+      }
+    case ADD_EVALUATION_ERROR:
+      return {
+        error:payload
+      }
+    case ADD_EVALUATION:
       return {
         pass: 'OK'
       }
