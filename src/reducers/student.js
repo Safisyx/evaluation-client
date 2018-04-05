f97309daeb552a289d54eab07219eacbed7142be
Safisyx <safisyx@gmail.com>
2018-04-06 00:28:22 +0200

    Handle the action afterPick

--- src/reducers/student.js
+++ src/reducers/student.js
@@ -1,9 +1,11 @@
-import {PICK_STUDENT} from '../actions/types'
+import {PICK_STUDENT, AFTER_PICK} from '../actions/types'
 
 export default (state = null, {type, payload}) => {
   switch (type) {
     case PICK_STUDENT:
       return payload
+    case AFTER_PICK:
+      return null
     default:
       return state
   }
