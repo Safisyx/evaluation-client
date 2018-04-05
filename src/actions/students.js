b8a19328cd9cce50b464c958c5c7ea204642a049
Safisyx <safisyx@gmail.com>
2018-04-05 12:08:42 +0200

    Add action to create a student

--- src/actions/students.js
+++ src/actions/students.js
@@ -1,6 +1,6 @@
 import * as request from 'superagent'
 import {baseUrl} from '../constants'
-import {DELETE_STUDENT} from './types'
+import {DELETE_STUDENT, ADD_STUDENT, ADD_STUDENT_ERROR} from './types'
 
 export const deleteStudent = (id) => (dispatch, getState) => {
   const state = getState()
@@ -19,4 +19,27 @@ export const deleteStudent = (id) => (dispatch, getState) => {
       })
     })
     .catch(err => console.error(err))
+}
+
+export const addStudent = ({name,photo}) => (dispatch, getState) => {
+  const state = getState()
+  const jwt = state.login.user
+  const batchId = state.batch.id
+
+  request
+    .post(`${baseUrl}/students/batches/${batchId}`)
+    .send({name,photo})
+    .set('Authorization', `Bearer ${jwt}`)
+    .then(result => {
+      dispatch({
+        type: ADD_STUDENT,
+        payload: result.body
+      })
+    })
+    .catch(err => {
+    		dispatch({
+    			type: ADD_STUDENT_ERROR,
+    			payload: err.response.body.message || 'Error'
+    		})
+    })
 }
