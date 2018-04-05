7ff7d3d9a58c162b44da6f29b11b02a7ad1ac2bd
Safisyx <safisyx@gmail.com>
2018-04-05 13:04:53 +0200

    Add action to edit a student

--- src/actions/students.js
+++ src/actions/students.js
@@ -1,6 +1,6 @@
 import * as request from 'superagent'
 import {baseUrl} from '../constants'
-import {DELETE_STUDENT, ADD_STUDENT, ADD_STUDENT_ERROR} from './types'
+import {DELETE_STUDENT, ADD_STUDENT, ADD_STUDENT_ERROR, EDIT_STUDENT} from './types'
 
 export const deleteStudent = (id) => (dispatch, getState) => {
   const state = getState()
@@ -41,5 +41,25 @@ export const addStudent = ({name,photo}) => (dispatch, getState) => {
     			type: ADD_STUDENT_ERROR,
     			payload: err.response.body.message || 'Error'
     		})
+    })
+}
+
+export const editStudent = (id, {name,photo}) => (dispatch, getState) => {
+  const state = getState()
+  const jwt = state.login.user
+  console.log('-------------------------');
+  console.log(id, name, photo);
+  request
+    .patch(`${baseUrl}/students/${id}`)
+    .send({name,photo})
+    .set('Authorization', `Bearer ${jwt}`)
+    .then(result => {
+      dispatch({
+        type: EDIT_STUDENT,
+        payload: result.body
+      })
+    })
+    .catch(err => {
+      console.error(err);
     })
 }
