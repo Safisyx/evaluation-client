ad2c49b144ba9f9c3894ecacc173148dae478a28
Safisyx <safisyx@gmail.com>
2018-04-06 10:59:08 +0200

    Work on css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import LoginPage from './components/login/LoginPage'
 import BatchList from './components/batches/BatchList'
 import BatchOverview from './components/batches/BatchOverview'
 import StudentPage from './components/students/StudentPage'
+import TopBar from './components/layouts/TopBar'
+import LogoutPage from './components/logout/LogOutPage'
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 
 class App extends Component {
@@ -11,10 +13,9 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <header className="App-header">
-            <h1 className="App-title">Student Evaluation Tool</h1>
-          </header>
+          <TopBar/>
           <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/logout" component={LogoutPage}/>
           <Route exact path="/batches" component={BatchList}/>
           <Route exact path="/batches/:id" component={BatchOverview}/>
           <Route exact path="/batches/:batchId/students/:studentId" component={StudentPage}/>
